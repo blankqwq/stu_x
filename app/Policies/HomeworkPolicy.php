467f912679d7cbfd7441c30b255b7e132aa83113
blankworld <1136589038@qq.com>
2018-12-10 21:11:08 +0800

    Update HomeworkPolicy.php

--- app/Policies/HomeworkPolicy.php
+++ app/Policies/HomeworkPolicy.php
@@ -1,15 +1,11 @@
 <?php
-
 namespace App\Policies;
-
 use App\Models\User;
 use App\Models\Homework;
 use Illuminate\Auth\Access\HandlesAuthorization;
-
 class HomeworkPolicy
 {
     use HandlesAuthorization;
-
     /**
      * Determine whether the user can view the homework.
      *
@@ -22,7 +18,6 @@ class HomeworkPolicy
         return $user->isClassOf($homework->class_id)||
             $user->hasRole(config('code.role').'|class'.$homework->class_id) ;
     }
-
     /**
      * Determine whether the user can create homeworks.
      *
@@ -33,7 +28,6 @@ class HomeworkPolicy
     {
         return true;
     }
-
     /**
      * Determine whether the user can update the homework.
      *
@@ -46,7 +40,6 @@ class HomeworkPolicy
         return $user->id === $homework->teacher_id||
             $user->hasRole(config('code.role').'|class'.$homework->class_id) ;
     }
-
     /**
      * Determine whether the user can delete the homework.
      *
